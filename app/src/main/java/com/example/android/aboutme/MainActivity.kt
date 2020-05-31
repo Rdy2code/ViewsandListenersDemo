ef67ca277602ef3fb53942ee783e30477df809db
Louis Wollenberger <lvwmailbox@yahoo.com>
2020-05-31 11:40:30 -0700

    Hide soft keyboard after clicking done

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.android.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -29,7 +31,8 @@ class MainActivity : AppCompatActivity() {
         editText.visibility = View.GONE
         view.visibility = View.GONE
         nickNameText.visibility = View.VISIBLE
-
+        //Hide keyboard
+        val methodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        methodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
-
 }
