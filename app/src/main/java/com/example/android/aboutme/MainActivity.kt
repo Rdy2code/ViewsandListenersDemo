d8d23811f52e0633178d199ae0db8ec66a246185
Louis Wollenberger <lvwmailbox@yahoo.com>
2020-05-31 11:32:53 -0700

    Add EditText field, button, and listener

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.android.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editText: EditText
+    private lateinit var nickNameText: TextView
+    private lateinit var doneButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.nickname_edit)
+        nickNameText = findViewById(R.id.nickname_text)
+        doneButton = findViewById(R.id.done_button)
+
+        doneButton.setOnClickListener {addNickName(it)}
     }
+
+    private fun addNickName(view: View) {
+        nickNameText.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nickNameText.visibility = View.VISIBLE
+
+    }
+
 }
